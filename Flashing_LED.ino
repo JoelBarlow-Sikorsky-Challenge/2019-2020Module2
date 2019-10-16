127894a4732e18f5fa46bcaeb9395e6f0a751bd3
Kol Crooks <kolcrooks10@gmail.com>
2019-10-16 16:29:59 -0400

    init commit with sketches

---
+++ Flashing_LED.ino
@@ -0,0 +1,1 @@
+/*  Flashing LED sketch*/#define LED_PIN 6int wait = 1000;void setup() {  pinMode(LED_PIN, OUTPUT);}void loop() {  digitalWrite(LED_PIN, HIGH);  delay(wait);  digitalWrite(LED_PIN, HIGH);  delay(wait);}
